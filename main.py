cad4aa128bb511171496122f30cef1d92fd369bc
WardHaddadin <wh293@cam.ac.uk>
2021-02-23 19:45:18 +0000

    one file

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-from src import best_twos, best_threes, best_fours
+from src import best_twos, best_threes, best_fours, read_data
+
+
+print(read_data.read_data('data/b_little_bit_of_everything.in')[4])
---
+++ src/bests.py
@@ -0,0 +1,57 @@
+
+
+def twos(pizza_dict):
+
+    num_pizza = len(pizza_dict.keys())
+    pairs = {}
+    
+    for i in range(num_pizza):
+        for j in range(i):
+        
+            len_pizza_1 = len(pizza_dict[i])
+            len_pizza_2 = len(pizza_dict[j])
+            distinct = len(set(pizza_dict[i] + pizza_dict[j]))
+            pairs[(i,j)] = [distinct, len_pizza_1, len_pizza_2]
+            
+    
+    return sorted(pairs.items(), key = lambda item: item[1][0])
+        
+
+def threes(pizza_dict):
+
+    num_pizza = len(pizza_dict.keys())
+    pairs = {}
+    
+    for i in range(num_pizza):
+        for j in range(i):
+            for k in range(j):
+                len_pizza_1 = len(pizza_dict[i])
+                len_pizza_2 = len(pizza_dict[j])
+                len_pizza_3 = len(pizza_dict[k])
+                
+                distinct = len(set(pizza_dict[i] + pizza_dict[j] + pizza_dict[k]))
+                pairs[(i,j,k)] = [distinct, len_pizza_1, len_pizza_2, len_pizza_3]
+            
+    
+    return sorted(pairs.items(), key = lambda item: item[1][0])
+    
+
+def fours(pizza_dict):
+
+    num_pizza = len(pizza_dict.keys())
+    pairs = {}
+    
+    for i in range(num_pizza):
+        for j in range(i):
+            for k in range(j):
+                for l in range(k):
+                    len_pizza_1 = len(pizza_dict[i])
+                    len_pizza_2 = len(pizza_dict[j])
+                    len_pizza_3 = len(pizza_dict[k])
+                    len_pizza_4 = len(pizza_dict[l])
+                    
+                    distinct = len(set(pizza_dict[i] + pizza_dict[j] + pizza_dict[k] + pizza_dict[l]))
+                    pairs[(i,j,k,l)] = [distinct, len_pizza_1, len_pizza_2, len_pizza_3, len_pizza_4]
+            
+    
+    return sorted(pairs.items(), key = lambda item: item[1][0])
