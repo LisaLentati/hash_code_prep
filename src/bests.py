5f247335f9750897d8503d51af0c95d542320acc
Lisa <lentatilisa@gmail.com>
2021-02-23 21:58:12 +0100

    added three_list function

--- src/bests.py
+++ src/bests.py
@@ -33,6 +33,25 @@ def twos_list(pizza_dict):
     return pairs
         
 
+def threes_list(pizza_dict):
+
+    num_pizza = len(pizza_dict.keys())
+    pairs = []
+    
+    for i in range(num_pizza):
+        for j in range(i):
+            for k in range(j):
+                len_pizza_1 = len(pizza_dict[i])
+                len_pizza_2 = len(pizza_dict[j])
+                len_pizza_3 = len(pizza_dict[k])
+                
+                distinct = len(set(pizza_dict[i] + pizza_dict[j] + pizza_dict[k]))
+                pairs.append([i, j, k, distinct, len_pizza_1+ len_pizza_2+ len_pizza_3 - distinct])
+            
+    
+    return pairs
+
+
 def threes(pizza_dict):
 
     num_pizza = len(pizza_dict.keys())
